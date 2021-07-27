3c067095de93d1ff398686600b9114d860ed5722
XuefengNi2021 <2464685N@student.gla.ac.uk>
2021-07-28 00:00:39 +0100

    chapter6 first commit

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
     # Refer to the TwD book for more information on how this updated view works.
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
