55b9c0dd905e37d8522027252c21a26fef15f4b0
XuefengNi2021 <2464685N@student.gla.ac.uk>
2021-07-27 14:53:18 +0100

    gerardo's suggestion

---
+++ rango/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+]
